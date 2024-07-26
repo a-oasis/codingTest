908e7abe8aeabf6f54a3005961c5a2626b240b2e
박영훈 <pyh007264@gmail.com>
2024-07-26 09:47:25 +0900

    불량 사용자 풀이 미완료

--- Solution.java
+++ Solution.java
@@ -1,14 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    public static int solution(String[] user_id,String[] banned_id){
+    public static int solution(String[] userIds,String[] bannedIds){
         //배열 copy by reference shallow copy 값 같이 바뀜
         int answer = 0;
+
+        ArrayList<String> candidates=new ArrayList<>();
         
-        for (int i = 0; i < user_id.length; i++) {
-            user_id[i]="d";
+        //후보 추출
+        //user_id 와 banned_id 유사하면 후보
+        for(int i=0;i<userIds.length;i++){
+            for(int j=0;j<bannedIds.length;j++){
+                if(userIds[i].equals(bannedIds[j])) candidates.add(userIds[i]);
+            }
         }
-        for ( String string : user_id) {
-            System.out.println(string);
+
+        for(int i=0;i<candidates.size();i++){
+            
         }
+        
         return answer;
     }
 
