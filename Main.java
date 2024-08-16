db9054efd2c2b41484933fa2a7c27d980a9d3654
박영훈 <pyh007264@gmail.com>
2024-08-16 17:34:15 +0900

    순열 완

--- Main.java
+++ Main.java
@@ -1,11 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Solution solution=new Solution();
+    static int N;
+    static boolean[] check;
+    static int[] checkArr;
+    static StringBuilder sb=new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
 
-        String[] str={"a","b"};
-        solution.solution(str, args);
+        checkArr=new int[N];
+        check=new boolean[N];
 
-        System.out.println(str[0]);
+        permu(0);
+        System.out.println(sb.toString());
 	}
+    
+    public static void permu(int tgt){
+        if(tgt==N){
+            for(int i=0;i<N;i++){
+                sb.append(checkArr[i]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+            return;  
+        }
+        for(int i=0;i<N;i++){
+            if(check[i]) continue;
+
+            check[i]=true;
+            checkArr[tgt]=i+1;
+            permu(tgt+1);
+            check[i]=false;
+        }
+    }
 }
--- Solution.java
+++ Solution.java
@@ -3,22 +3,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    static class Comparator{
+        static public boolean equals(String id,String compareId){
+            if(id.length()!=compareId.length()) return false;
+
+            for(int i=0;i<compareId.length();i++){
+                if(id.charAt(i)=='*') continue;
+                if(id.charAt(i)!=compareId.charAt(i)) return false;
+            }
+            return true;
+        }
+    }
+
     public static int solution(String[] userIds,String[] bannedIds){
         //배열 copy by reference shallow copy 값 같이 바뀜
         int answer = 0;
 
-        ArrayList<String> candidates=new ArrayList<>();
+        List<List<Integer>> candidateList=new ArrayList<>(userIds.length);
         
         //후보 추출
         //user_id 와 banned_id 유사하면 후보
         for(int i=0;i<userIds.length;i++){
+            List<Integer> candidates=new ArrayList<>();
             for(int j=0;j<bannedIds.length;j++){
-                if(userIds[i].equals(bannedIds[j])) candidates.add(userIds[i]);
+                if(Comparator.equals(userIds[i],bannedIds[j])){
+                    candidates.add(j);
+                }
             }
+            candidateList.add(candidates);
         }
 
-        for(int i=0;i<candidates.size();i++){
-            
+        for(int i=0;i<userIds.length;i++){
+            if(userIds[i].equals(candidateList))
         }
         
         return answer;
